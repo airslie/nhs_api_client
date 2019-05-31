377d6df2e7d0158ebdee9e0207718bb89ece9994
Tim Crowe <tim@woodpigeon.com>
2019-05-31 18:14:49 +0100

    Support new client options
    last_change_date
    page_size
    quit_after args
    
    Example usage:
    
    client.fetch_pages(
      roles: :practices,
      last_change_date: "2019-05-25",
      page_size: 5,
      quit_after: 6) do ..

--- go.rb
+++ go.rb
@@ -13,16 +13,14 @@ end
 
 client = NHSApiClient::Organisations::Client.new
 
-roles = %i(practices) # also e.g. branch_surgeries
-
 # Fetch each page
-client.fetch_pages(roles: roles, last_change_date: nil) do |page|
+client.fetch_pages(roles: :practices, last_change_date: "2019-05-25", page_size: 5, quit_after: 6) do |page|
   # For each organisation in the page..
   page.items.each do |item|
     # Output it's name...
     puts item.name
     # ...then implicity fetch its extended details from another url so we can get the tel no.
-    puts "  Tel: #{item.details.tel}"
+    # puts "  Tel: #{item.details.tel}"
   end
   puts "#{page.offset + page.item_count} of #{page.total_count}"
 end
--- lib/nhs_api_client/organisations/client.rb
+++ lib/nhs_api_client/organisations/client.rb
@@ -6,33 +6,48 @@ module NHSApiClient
   module Organisations
     class Client
       include HTTParty
-      BASE_URL = "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations"
-      FIRST_PAGE_URLS = {
-        practices: "#{BASE_URL}?PrimaryRoleId=RO177&Limit=100",
-        branch_surgeries: "#{BASE_URL}?PrimaryRoleId=RO96&Limit=100"
-      }.freeze
+      BASE_URL = "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations"\
+                 "?PrimaryRoleId=<primary_role_id>"\
+                 "&Limit=<page_size>"\
+                 "&LastChangeDate=<last_change_date>"
+      ROLE_CODES = { practices: "RO177", branch_surgeries: "RO96" }.freeze
+      DEFAULT_PAGE_SIZE = 100
+      DEFAULT_LAST_CHANGE_DATE = ""
 
-      # last_change_date: nil returns all
-      def fetch_pages(roles:, **_options)
+      # quit_after is a kill switch that stops processing when the specificed number of
+      # records (not pages) have been found. Useful when debugging to avoid hitting the API
+      # excessively. eg quit_after: 10
+      # roles can be an array of symbols or just one
+      def fetch_pages(roles:, **options)
         Array(roles).each do |role|
-          page = start_page_for(role)
+          page = PageTheFirst.new(initial_url_for(role, **options))
+          quit_after = options[:quit_after].to_i
           item_count = 0
           loop do
-            GC.start # force garbage collection to prevent exessive memory usage
+            GC.start # force garbage collection to prevent excessive memory usage
             page = page.next
             item_count += page.item_count
             yield(page) if block_given?
             break if page.next_url.nil?
-            break if item_count >= 100
+            break if quit_after.positive? && item_count >= quit_after
           end
         end
       end
 
-      def start_page_for(role)
-        PageTheFirst.new(FIRST_PAGE_URLS.fetch(role))
-        # OpenStruct.new(
-        #   next_url: FIRST_PAGE_URLS.fetch(role)
-        # )
+      private
+
+      # Options:
+      # - :last_change_date - if nil it will returns all records
+      # - :page_size - the number of practices per page
+      def initial_url_for(role, **options)
+        last_change_date = options.fetch(:last_change_date, DEFAULT_LAST_CHANGE_DATE)
+        page_size = options.fetch(:page_size, DEFAULT_PAGE_SIZE)
+        role_code = ROLE_CODES.fetch(role.to_sym)
+
+        BASE_URL
+          .gsub("<last_change_date>", last_change_date)
+          .gsub("<primary_role_id>", role_code)
+          .gsub("<page_size>", page_size.to_s)
       end
     end
   end
